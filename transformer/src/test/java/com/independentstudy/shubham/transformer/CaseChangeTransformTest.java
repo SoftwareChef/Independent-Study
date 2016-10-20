62c810922710826054cb027e6e57fb801984ef2b
Shubham Mahajan <shubham.mahajan@icloud.com>
2016-10-19 21:20:54 -0400

    Case Change Transform class and test running
    Requires comments and checkstyle to be done

--- transformer/src/test/java/com/independentstudy/shubham/transformer/CaseChangeTransformTest.java
+++ transformer/src/test/java/com/independentstudy/shubham/transformer/CaseChangeTransformTest.java
@@ -8,14 +8,34 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
- * @author User
+ * @author Shubham Mahajan
  *
  */
 public class CaseChangeTransformTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testForwardTransform() {
+		CaseChangeTransform cct = new CaseChangeTransform();
+		assertEquals("shubhAM", cct.forwardTransform("SHUBHam"));
+		assertEquals("4", cct.forwardTransform("4"));
+		assertEquals("4ab", cct.forwardTransform("4AB"));
+		assertEquals("4EVER", cct.forwardTransform("4ever"));
+		assertEquals("4ever", cct.forwardTransform("4EVER"));
+		assertEquals("EVER", cct.forwardTransform("ever"));
+		assertEquals("shubham", cct.forwardTransform("SHUBHAM"));
+		assertEquals("SHUBHAM", cct.forwardTransform("shubham"));
+	}
+	@Test
+	public void testReverseTransform() {
+		CaseChangeTransform cct = new CaseChangeTransform();
+		assertEquals("shubhAM", cct.forwardTransform("SHUBHam"));
+		assertEquals("4", cct.forwardTransform("4"));
+		assertEquals("4ab", cct.forwardTransform("4AB"));
+		assertEquals("4EVER", cct.forwardTransform("4ever"));
+		assertEquals("4ever", cct.forwardTransform("4EVER"));
+		assertEquals("EVER", cct.forwardTransform("ever"));
+		assertEquals("shubham", cct.forwardTransform("SHUBHAM"));
+		assertEquals("SHUBHAM", cct.forwardTransform("shubham"));
 	}
 
 }
